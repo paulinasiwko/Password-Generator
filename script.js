6f5f9cbc6632537a4884c218cfcca4d33c119a03
Paulina Siwko <pau.siwko@gmail.com>
2023-12-15 21:02:44 +0100

    added javascript to generate password

--- script.js
+++ script.js
@@ -90,17 +90,63 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+  var lengthOfPassword = prompt('How many characters do you want in your password?');
+  lengthOfPassword = +lengthOfPassword;
+  if (isNaN(lengthOfPassword) || lengthOfPassword < 8 || lengthOfPassword > 128) {
+    alert('Please enter a valid number between 8 and 128');
+  } else {
+    var lowerCase = confirm('Click OK if you want to have lower cases in you password.');
+    var upperCase = confirm('Click OK if you want to have upper cases in you password.');
+    var numeric = confirm('Click OK if you want to have numeric cases in you password.');
+    var specialCharacters = confirm('Click OK if you want to have special characters in you password.');
+  
+    var userChoices = [lengthOfPassword, lowerCase, upperCase, numeric, specialCharacters];  
+  }
 
+  return userChoices;
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+  var randomMax = arr.length;
+  return Math.floor(Math.random() * (arr.length));
 }
 
 // Function to generate password with user input
 function generatePassword() {
+  var userInput = getPasswordOptions();
+  console.log(userInput)
+  if (userInput === undefined) {
+    return;
+  }
 
+  var password = [];
+  var arrayChoice = [];
+
+  if (userInput[1]) {
+    arrayChoice.push(lowerCasedCharacters);
+  } 
+
+  if (userInput[2]) {
+    arrayChoice.push(upperCasedCharacters);
+  }
+
+  if (userInput[3]) {
+    arrayChoice.push(numericCharacters);
+  }
+
+  if (userInput[4]) {
+    arrayChoice.push(specialCharacters);
+  }
+
+  for (var i = 0; i < userInput[0]; i++) {
+    var pickArray = Math.floor(Math.random() * arrayChoice.length);
+    var selectedArray = arrayChoice[pickArray];
+    var randomIndex = getRandom(selectedArray);
+    password.push(selectedArray[randomIndex]);
+  }
+
+  return password.join('');
 }
 
 // Get references to the #generate element
